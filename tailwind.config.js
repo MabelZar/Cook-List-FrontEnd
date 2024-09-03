0d5cfe4060397de193455c4158a36cbcd5bf6b90
MabelZar <mabel.zar.esp@gmail.com>
2024-09-03 21:09:15 +0200

    configuration created

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.export = withMT({
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}', 
+    './src/.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        'inset-custom': 'inset 0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
+      },
+    },
+  },
+  plugins: [],
+});
+
